72b06b4c4b0db33b4598d4a202343136a693edd2
shinnakashima08-cmd <shinnakashima08@gmail.com>
2026-05-05 18:46:43 -0300

    Actualizar calculadora.py
    versión 2

--- calculadora.py
+++ calculadora.py
@@ -1,51 +1,36 @@
 print("Bienvenido a mi calculadora")
 
-while true: 
+while True:
+    print("¿qué operación querés realizar?")
+    print("1. suma")
+    print("2. resta")
+    print("3. multiplicación")
+    print("4. división")
 
-print("¿qué operación quierés realizar?")
-print("1. suma")   
-print("2. resta")
-print("3. multiplicación")
-print("4. división")
+    opcion = input("elegí una opción (1/2/3/4): ")
 
-opcion = input("elegi una opción (1/2/3/4): ")
+    numero1 = float(input("Ingresá el primer número: "))
+    numero2 = float(input("Ingresá el segundo número: "))
 
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    multiplicacion = numero1 * numero2
 
-numero1 = float(input("Ingresá el primer número: "))
-numero2 = float(input("Ingresá el segundo número: "))
-
-print("Número 1:", numero1)
-print("Número 2:", numero2)
-
-suma = numero1 + numero2
-
-
-resta = numero1 - numero2
-
-
-multiplicacion = numero1 * numero2
-
-
-
-
-
-if opcion == "1":
-    print("La suma es:", round(suma, 2))
-elif opcion == "2":
-    print("La resta es:", round(resta, 2))
-elif opcion == "3":
-    print("la multiplicación es:", round(multiplicacion, 2))
-elif opcion == "4":
-    if numero2 == 0:
-        print("Error: no se puede dividir por cero")
+    if opcion == "1":
+        print("La suma es:", round(suma, 2))
+    elif opcion == "2":
+        print("La resta es:", round(resta, 2))
+    elif opcion == "3":
+        print("La multiplicación es:", round(multiplicacion, 2))
+    elif opcion == "4":
+        if numero2 == 0:
+            print("Error: no se puede dividir por cero")
+        else:
+            division = numero1 / numero2
+            print("La división es:", round(division, 2))
     else:
-        division = numero1 / numero2
-        print("La división es:", round(division, 2))
-        
-else:
-    print("operacón invalisa") 
+        print("Opción inválida")
 
-continuar = input("¿querés realizar otra operación? (si/no): ")
-
-if continuar = "no": 
-break 
+    continuar = input("¿Querés hacer otra operación? (si/no): ")
+    if continuar == "no":
+        break
